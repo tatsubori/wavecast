e7655e6466c67e191adccf03226c4b396c6c5d0e
Michiaki Tatsubori <mich@acm.org>
2013-10-20 07:29:49 +0300

    Using $_SERVER['SERVER_NAME'] instead of $_SERVER['SERVER_ADDR'].

--- Wavecast/htdocs/wave/podcast/iepghook.php
+++ Wavecast/htdocs/wave/podcast/iepghook.php
@@ -1,6 +1,6 @@
 <?php
-$host = $_SERVER['SERVER_ADDR'];
-$scriptURL = 'http://' . $_SERVER['SERVER_ADDR'] . $_SERVER['PHP_SELF'];
+#$scriptURL = 'http://' . $_SERVER['SERVER_ADDR'] . $_SERVER['PHP_SELF'];
+$scriptURL = "http://${_SERVER['SERVER_NAME']}${_SERVER['PHP_SELF']}";
 if (! isset($_REQUEST['target'])) {
 	$targetURL = 'http://tv.so-net.ne.jp/chart/23.action';
 } else {
@@ -24,18 +24,17 @@ $html = file_get_contents($targetURL);
 # accesses to iepg files
 $html = preg_replace_callback('|href="/iepg\\.tvpi\\?id=(\d+)"|i',
 		function($matches) {
-			$retargetURLBase = 'http://' . $_SERVER['SERVER_ADDR'] . '/wave/jp/vidrecsrc.php?proc=iepg&iepg=';
-			return 'href="' . $retargetURLBase . urlencode($matches[1]) . '"'; 
+			return "href=\"http://${_SERVER['SERVER_NAME']}/wave/jp/vidrecsrc.php?proc=iepg&iepg=" .
+				urlencode('http://tv.so-net.ne.jp/iepg.tvpi?id=' + $matches[1]) .
+				'"'; 
 		}, $html);
-#$html = preg_replace('|href="\\s*\\?head=(\d+)\\s*"|i',
-#		'href="' . $scriptURL . "?target=${targetURLBase}" . '?head=${1}"', $html);
 # links to other pages
 $html = preg_replace('|a\\s+href="\\s*(\\?[^"]*)\\s*"|i',
-		'a href="' . $scriptURL . "?target=${targetURLBase}" . '${1}"', $html);
+		"a href=\"${scriptURL}?target=${targetURLBase}" . '${1}"', $html);
 $html = preg_replace('|a\\s+href="\\s*(/[^"]*)\\s*"|i',
-		'a href="' . $scriptURL . "?target=http://${targetHost}" . '${1}"', $html);
+		"a href=\"${scriptURL}?target=http://${targetHost}" . '${1}"', $html);
 $html = preg_replace('|action="\\s*(/[^"]*)\\s*"([^>]*>)|i',
-		'action="' . $scriptURL . '"${2}' . "\n"
+		"action=\"${scriptURL}" . '"${2}' . "\n"
 			. '<input type="hidden" name="target" value="http://' . $targetHost . '${1}" />', $html);
 $html = str_replace("<head>", "<head>\n<base href=\"${targetURL}\" />", $html);
 echo $html;
